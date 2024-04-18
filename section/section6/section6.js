fdb4b1c1386a4893fffec93941c285505add0c93
生野ななほ <nana@ikunonanahonoMacBook-Air.local>
2024-04-18 09:05:34 +0900

    section6課題

--- section/section6/section6.js
+++ section/section6/section6.js
@@ -1,3 +1,12 @@
-const sum = null;
+const sum = (x, y) => {
+    let sum = 0;
+    const min = Math.min(x, y);
+    const max = Math.max(x, y);
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
+}
 
 module.exports = { sum };
