7524bfd7554361037239a8eff63c25d86c981ed6
生野ななほ <nana@ikunonanahonoMacBook-Air.local>
2024-04-25 10:57:46 +0900

    section8課題

--- section/section8/section8.js
+++ section/section8/section8.js
@@ -1,13 +1,42 @@
-const firstIndex = null;
-const countArrayLength = null;
+const firstIndex = (array) => {
+  const first = array[0];
+  return first;
+};
 
-const extractArray = null;
+const countArrayLength = (array) => {
+  if (!Array.isArray(array)) {
+    return "これは配列ではありません";
+  } else if (array.length === 0) {
+    return "この配列は空配列です";
+  } else {
+    return "この配列は"+array.length+"個のデータを持っています";
+  }
+};
 
-const makeMark = null;
+const extractArray = (array) => {
+  return array.slice(1, 3);
+};
 
-const onlyString = null;
+const makeMark = (mark, number) => {
+  const result =[];
+  for (let i = 0; i < number; i++) {
+    result.push(mark);
+  }
+  return result;
+};
 
-const addNumber = null;
+const onlyString = (array) => {
+  return array.filter((item) => typeof item === "string");
+};
+
+const addNumber = (array) => {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const newString = `${i+1}: ${array[i]}`;
+    newArray.push(newString);
+  }
+  return newArray;
+};
 
 module.exports = {
   firstIndex,
